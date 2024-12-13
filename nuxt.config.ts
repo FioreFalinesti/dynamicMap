35bb6575e2d78a57bbffa6ab2625f9233768b68d
Fiore Falinesti <f.falinesti@gmail.com>
2024-12-12 17:17:35 -0800

    nuxt config

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+import Aura from "@primevue/themes/aura";
+import Material from "@primevue/themes/material";
+import Lara from "@primevue/themes/lara";
+import Nora from "@primevue/themes/nora";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: {
+    enabled: true,
+  },
+  typescript: {
+    strict: false,
+  },
+  modules: ["@primevue/nuxt-module"],
+  primevue: {
+    /* PrimeVue Configuration */
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+  runtimeConfig: {
+    // https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
+  },
+  $production: {
+    // https://nuxt.com/docs/getting-started/configuration#environment-overrides
+  },
+  $development: {},
+  $env: {
+    staging: {},
+  },
+});
